597fee4ef3f4ef9d098deb5a4bd66f1c42af2433
Jutololla <julianangulop@gmail.com>
2021-09-16 19:52:14 -0500

    Refactor Component StoreProvider

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
-import React, {useReducer} from 'react';
+import React from 'react';
 import Form from './Components/Form';
-import {Store, initialState} from './Components/Form';
 import List from './Components/List';
+import StoreProvider from './Components/StoreProvider';
 
 
 
@@ -13,53 +13,10 @@ import List from './Components/List';
 
 
 
-export function reducer(state, action) {
-  switch (action.type) {
-    case 'update-item':
-      const todoUpItem = state.todo;
-      const listUpdateEdit = todoUpItem.list.map((item) => {
-        if (item.id === action.item.id) {
-          return action.item;
-        }
-        return item;
-      });
-      todoUpItem.list = listUpdateEdit;
-      todoUpItem.item = {};
-      return { ...state, todo: todoUpItem }
-    case 'delete-item':
-      const todoUpDelete = state.todo;
-      const listUpdate = todoUpDelete.list.filter((item) => {
-        return item.id !== action.id;
-      });
-      todoUpDelete.list = listUpdate;
-      return { ...state, todo: todoUpDelete }
-    case 'update-list':
-      const todoUpList = state.todo;
-      todoUpList.list = action.list;
-      return { ...state, todo: todoUpList }
-    case 'edit-item':
-      const todoUpEdit = state.todo;
-      todoUpEdit.item = action.item;
-      return { ...state, todo: todoUpEdit }
-    case 'add-item':
-      const todoUp = state.todo.list;
-      todoUp.push(action.item);
-      return { ...state, todo: {list: todoUp, item: {}} }
-    default:
-      return state;
-  }
-}
 
 
 
-export const StoreProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
 
-  return <Store.Provider value={{ state, dispatch }}>
-    {children}
-  </Store.Provider>
-
-}
 
 
 
